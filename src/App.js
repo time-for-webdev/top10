e68c19c516171205a62be19017a8532f7ba71b99
prince raj <@gmail.com>
2023-03-13 15:33:01 +0530

    project started navbar created for home-page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./component/Home/Home";
 import About from "./component/about/About";
+import Navbar from "./component/Navbar/Navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
