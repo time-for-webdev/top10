a414e3c96ff770d7a34d74e9a831669e3d2701d7
prince raj <@gmail.com>
2023-03-13 10:14:13 +0530

    routes added and new pages linked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./component/Home/Home";
+import About from "./component/about/About";
 function App() {
   return (
-    <div className="App">
-      <h1>by harsh</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
