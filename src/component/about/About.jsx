f26ce975c03f22332e5568b453b71eaa8e32381f
paras-sharma-github <sparas452@gmail.com>
2023-03-15 18:49:08 +0530

    aboutpage started

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import styles from "./About.module.css";
+import { useState } from "react";
 const About = () => {
-  return <div>About</div>;
+  //const [linkClass, setlinkClass] = useState();
+  const changeActive=(e)=>{
+    e.preventDefault();
+    document.getElementsByClassName(styles.active).classList.add(styles.activer)
+  }
+  return <>
+       <section className={styles.page_nav}></section>
+       <div className={styles.page_nav_link}>
+        <ul>
+        <li><a className={""} onClick={(e)=>changeActive(e)} href="#">We Are</a></li>
+        <li><a className={styles.active} onClick={(e)=>changeActive(e)} href="#">Our Values</a></li>
+        <li><a className={""} onClick={(e)=>changeActive(e)} href="#">Review Methodology</a></li>
+        <li><a className={""} onClick={(e)=>changeActive(e)} href="#">The Research Lab</a></li>
+       </ul>
+       </div>
+       
+  </>;
 };
 
 export default About;
