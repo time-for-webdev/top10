01fff98132af26a16c5dfb165850994abe28625a
hojha235 <hojha235@gmail.com>
2023-03-17 09:54:52 +0530

    Card component made by Harsh

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./component/Home/Home";
 import About from "./component/about/About";
 import Navbar from "./component/Navbar/Navbar";
+import Card from "./component/Card/Card";
 function App() {
   return (
     <>
       <Navbar />
+      <Card/>
+      
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
