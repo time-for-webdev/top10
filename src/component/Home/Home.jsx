b4efeed4394aa4dbbade4eb3d0b0e7920fe089a8
prince raj <@gmail.com>
2023-03-18 19:57:57 +0530

    Vpn Card added and background setup

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -3,12 +3,28 @@ import Navbar from "../Navbar/Navbar";
 import styles from "./Home.module.css";
 import Top7 from "../top7/Top7";
 import Discloser from "../Discloser/Discloser";
+import Top10btm from "../top10bottom/Top10btm";
+import Card from "../Card/Card";
+import web from "../../assects/Home/header_background_people_default.svg";
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Discloser />
-      <Top7 />
+      <div className={styles.home_container}>
+        <Discloser />
+        <div className={styles.first_box}>
+          <div className={styles.first_left}>
+            <Top7 />
+            <Top10btm />
+          </div>
+          <img src={web} />
+        </div>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </>
   );
 };
---
+++ src/component/top10bottom/Top10btm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Top10btm.module.css";
+import web from "../../assects/Home/ppc-hero-list.svg";
+import web2 from "../../assects/Home/calender.svg";
+
+const Top10btm = () => {
+  return (
+    <div className={styles.bottom_container}>
+      <div className={styles.btm_wrap}>
+        <div className={styles.btm_para}>vpnMentor has been featured on:</div>
+        <img src={web} />
+        <div className={styles.updateDate}>
+          <img src={web2} />
+          <p>Updated on 16th March 2023</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Top10btm;
