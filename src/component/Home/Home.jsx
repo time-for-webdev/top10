cdb54eed53a4bf0e83465ccc30f4e5db72a5356e
prince raj <@gmail.com>
2023-03-19 21:55:59 +0530

    Rate us section done

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -6,6 +6,8 @@ import Discloser from "../Discloser/Discloser";
 import Top10btm from "../top10bottom/Top10btm";
 import Card from "../Card/Card";
 import web from "../../assects/Home/header_background_people_default.svg";
+import Category from "../Category/Category";
+import Rate from "../rate/Rate";
 const Home = () => {
   return (
     <>
@@ -24,6 +26,8 @@ const Home = () => {
         <Card />
         <Card />
         <Card />
+        <Rate />
+        <Category />
       </div>
     </>
   );
